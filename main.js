bd77d3392ffe7927554841c45834802f026a3528
Stevenmdo7 <steven.montesdeoca.noc3g@gmail.com>
2023-10-16 06:56:52 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,220 @@
+const pacientesAlmacenados = localStorage.getItem('datosPacientes');
+let pacientes = pacientesAlmacenados ? JSON.parse(pacientesAlmacenados) : [];
+
+if (pacientes.length === 0) {
+    pacientes = [
+        {
+            nombre: "Homero J.",
+            apellido: "Simpson",
+            edad: 36,
+            direccion: "Calle siempreviva 742",
+            diagnostico: "Crayon en el cerebro",
+            fechaHoraIngreso: ""
+        },
+        {
+            nombre: "Peter",
+            apellido: "Parker",
+            edad: 16,
+            direccion: "69th Road, en Forest Hills",
+            diagnostico: "Delirios de poderes por picadura de araña radioactiva",
+            fechaHoraIngreso: ""
+        },
+        {
+            nombre: "Bruce",
+            apellido: "Wayne",
+            edad: 40,
+            direccion: "Mansion Wayne",
+            diagnostico: "quiroptofobia.",
+            fechaHoraIngreso: ""
+        }
+    ];
+}
+
+function mostrarMensaje(mensaje, bgColor, textColor) {
+    const contenedorMensaje = document.getElementById("contenedor-mensaje");
+    contenedorMensaje.innerHTML = mensaje;
+    contenedorMensaje.style.backgroundColor = bgColor;
+    contenedorMensaje.style.color = textColor;
+    contenedorMensaje.style.display = "block";
+    contenedorMensaje.style.animation = "animacion-mensaje 2.5s ease-out";
+    setTimeout(function() {
+        contenedorMensaje.style.display = "none";
+    }, 2500);
+}
+
+const credencialesOriginales = {
+    usuario: "Coder",
+    contrasena: "lapelucaloca123"
+};
+
+function iniciarSesion(usuario, contraseña) {
+    const usuarioEncontrado = usuariosRegistrados.find(user => user.username === usuario && user.contrasena === contraseña);
+    if (usuarioEncontrado || (usuario === credencialesOriginales.usuario && contraseña === credencialesOriginales.contrasena)) {
+        return true;
+    }
+    return false;
+}
+
+function agregarPaciente(nombre, apellido, edad, direccion, diagnostico) {
+    if (!nombre || !apellido || isNaN(edad) || !direccion || !diagnostico) {
+        mostrarMensaje("Por favor, ingrese datos válidos para el paciente.", "#dc3545", "#fff");
+        return false;
+    }
+    const fechaHoraIngreso = new Date();
+    const horaIngreso = `${fechaHoraIngreso.getHours()}:${fechaHoraIngreso.getMinutes()} ${fechaHoraIngreso.getHours() >= 12 ? 'PM' : 'AM'}`;
+    const fechaIngreso = `${fechaHoraIngreso.getDate()}/${fechaHoraIngreso.getMonth() + 1}/${fechaHoraIngreso.getFullYear()}`;
+    pacientes.push({
+        nombre,
+        apellido,
+        edad,
+        direccion,
+        diagnostico,
+        fechaHoraIngreso: `${fechaIngreso} ${horaIngreso}`
+    });
+    mostrarMensaje("Paciente agregado con éxito.", "#17a2b8", "#fff");
+    actualizarListaPacientes();
+    localStorage.setItem('datosPacientes', JSON.stringify(pacientes));
+    return true;
+}
+
+function eliminarPaciente(indice) {
+    pacientes.splice(indice, 1);
+    actualizarListaPacientes();
+    localStorage.setItem('datosPacientes', JSON.stringify(pacientes));
+}
+
+function actualizarListaPacientes() {
+    const listaPacientes = document.getElementById("lista-pacientes");
+    listaPacientes.innerHTML = "";
+
+    pacientes.forEach((paciente, indice) => {
+        const itemPaciente = document.createElement("li");
+        itemPaciente.innerHTML = `
+            <div>
+                <b>${paciente.nombre} ${paciente.apellido}</b><br>
+                Edad: ${paciente.edad}<br>
+                Hora y Fecha de Ingreso: ${paciente.fechaHoraIngreso}<br>
+                Diagnóstico: ${paciente.diagnostico}
+            </div>
+            <button class="eliminar-paciente" data-indice="${indice}">Eliminar</button>
+        `;
+
+        const botonEliminar = itemPaciente.querySelector(".eliminar-paciente");
+        botonEliminar.addEventListener("click", function() {
+            const indice = this.getAttribute("data-indice");
+            eliminarPaciente(indice);
+            mostrarMensaje("Paciente eliminado con éxito.", "#581845", "#F3FF7A");
+        });
+
+        listaPacientes.appendChild(itemPaciente);
+    });
+}
+
+function cerrarSesion() {
+    const contenedorLogin = document.querySelector(".contenedor-login");
+    contenedorLogin.style.display = "block";
+    const contenedorAdmin = document.querySelector(".contenedor-admin");
+    contenedorAdmin.style.display = "none";
+    document.getElementById("usuario").value = "";
+    document.getElementById("contraseña").value = "";
+    document.getElementById("contenedor-mensaje").style.display = "none";
+}
+
+function Usuario(nombre, edad, email, sexo, username, contrasena) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.email = email;
+    this.sexo = sexo;
+    this.username = username;
+    this.contrasena = contrasena;
+}
+
+const usuariosRegistrados = [];
+
+document.getElementById("boton-registrarse").addEventListener("click", function () {
+    const contenedorLogin = document.querySelector(".contenedor-login");
+    contenedorLogin.style.display = "none";
+    const contenedorRegistro = document.querySelector(".contenedor-registro");
+    contenedorRegistro.style.display = "block";
+});
+
+document.getElementById("formulario-registro").addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const edad = document.getElementById("edad").value;
+    const email = document.getElementById("email").value;
+    const sexo = document.getElementById("sexo").value;
+    const username = document.getElementById("username").value;
+    const contrasena = document.getElementById("contrasena").value;
+    const confirmarContrasena = document.getElementById("confirmar-contrasena").value;
+
+    if (contrasena !== confirmarContrasena) {
+        mostrarMensaje("Las contraseñas no coinciden. Intente de nuevo.", "#dc3545", "#fff");
+        return;
+    }
+
+    const nuevoUsuario = new Usuario(nombre, edad, email, sexo, username, contrasena);
+    if (usuariosRegistrados.find(user => user.username === nuevoUsuario.username)) {
+        mostrarMensaje("Ese usuario ya existe, sé un poco más original.", "#dc3545", "#fff");
+    } else {
+        usuariosRegistrados.push(nuevoUsuario);
+        mostrarMensaje(`Se ha registrado al usuario ${username} correctamente.`, "#28a745", "#fff");
+     
+        document.getElementById("nombre").value = "";
+        document.getElementById("edad").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("sexo").value = "";
+        document.getElementById("username").value = "";
+        document.getElementById("contrasena").value = "";
+        document.getElementById("confirmar-contrasena").value = "";
+
+        const contenedorLogin = document.querySelector(".contenedor-login");
+        contenedorLogin.style.display = "block";
+        const contenedorRegistro = document.querySelector(".contenedor-registro");
+        contenedorRegistro.style.display = "none";
+    }
+});
+
+document.getElementById("formulario-login").addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    const usuario = document.getElementById("usuario").value;
+    const contraseña = document.getElementById("contraseña").value;
+
+    if (iniciarSesion(usuario, contraseña)) {
+        const contenedorLogin = document.querySelector(".contenedor-login");
+        contenedorLogin.style.display = "none";
+        const contenedorAdmin = document.querySelector(".contenedor-admin");
+        contenedorAdmin.style.display = "block";
+        actualizarListaPacientes();
+    } else {
+        mostrarMensaje("Credenciales incorrectas. Intente de nuevo.", "#dc3545", "#fff");
+    }
+});
+
+document.getElementById("boton-cerrar-sesion").addEventListener("click", cerrarSesion);
+
+document.getElementById("boton-agregar-paciente").addEventListener("click", function () {
+    const superposicion = document.getElementById("superposicion");
+    superposicion.style.display = "block";
+    document.getElementById("formulario-paciente").style.display = "block";
+});
+
+document.getElementById("cerrarFormulario").addEventListener("click", function () {
+    const superposicion = document.getElementById("superposicion");
+    superposicion.style.display = "none";
+    document.getElementById("formulario-paciente").style.display = "none";
+});
+
+document.getElementById("confirmarPaciente").addEventListener("click", function () {
+    const nombre = document.getElementById("nombre-paciente").value;
+    const apellido = document.getElementById("apellido-paciente").value;
+    const edad = document.getElementById("edad-paciente").value;
+    const direccion = document.getElementById("direccion-paciente").value;
+    const diagnostico = document.getElementById("diagnostico-paciente").value;
+
+    if (agregarPaciente(nombre, apellido, edad, direccion, diagnostico)) {
+        const superposicion = document.getElementById("superposicion");
+        superposicion.style.display = "none";
+        document.getElementById("formulario-paciente").style.display = "none";
+    }
+});
